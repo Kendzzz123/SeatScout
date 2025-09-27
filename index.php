40bd5b7775c5365c5caa766dba3a6813023a0009
Kendzzz123 <koadanza0014qc@student.fatima.edu.ph>
2025-09-27 13:48:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <ul>
+      <li><img src="" alt="logo-pic"></li>
+      <li><a href="">Explore</a></li>
+      <li><a href="">Favorites</a></li>
+      <li><a href="">My Cart</a></li>
+      <li><a href="">Help</a></li>
+      <li><a href="">Log-in/Register</a><img src="" alt="login-image"></li>
+    </ul>
+  </header>
+  <!--Grid Row: 1-->
+    <a href="">Category</a>
+    <a href="">Category</a>
+    <a href="">Category</a>
+    <a href="">Category</a>
+
+  <!--Carousel Sample(pwede tanggalin later)-->
+
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!--Grid Row: 1-->
+    <a href=""><img src="" alt="Loc-button"></a>
+    <a href="">Location</a>
+    <a href="">Dates</a>
+
+<!--Temporary Shtick-->
+
+<div>
+<h2>Name of Event</h2>
+
+<button>Location</button>
+<button>Dates</button>
+<button>Price</button>
+
+<section>
+  <div>
+  <h3>
+    Date
+  </h3>
+
+  <p>Event Name</p>
+  <p>Time</p>
+  <p>Description</p>
+  </div>
+</section>
+
+<section>
+  <div>
+  <h3>
+    Date
+  </h3>
+
+  <p>Event Name</p>
+  <p>Time</p>
+  <p>Description</p>
+  </div>
+</section>
+
+<section>
+  <div>
+  <h3>
+    Date
+  </h3>
+
+  <p>Event Name</p>
+  <p>Time</p>
+  <p>Description</p>
+  </div>
+</section>
+
+<aside>
+  <div>
+    <a href="">Category</a>
+    <a href="">Genre</a>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Vestibulum sit amet lacus diam. Aenean vel egestas eros.
+      Sed vel euismod nibh. Vivamus aliquet varius turpis, a varius neque dignissim quis.
+      Ut posuere sem ut dui sodales sagittis. Curabitur suscipit nisl quis luctus imperdiet.
+      Suspendisse id maximus nisl. Vestibulum ut nisl at sapien lacinia cursus vel fermentum nunc.
+      Nunc sapien odio, mattis eget suscipit dictum, gravida in metus.
+      Praesent felis justo, pellentesque sit amet semper a, iaculis quis neque.
+      Vivamus vestibulum orci a viverra lobortis.
+    </p>
+  </div>
+</aside>
+
+</div>
+</body>
+</html>
